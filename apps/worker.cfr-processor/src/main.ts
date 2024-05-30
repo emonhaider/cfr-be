b00f56f8c20f14e1b9ec324ddb43f3bca55d6fef
HH <habib.haider@gmail.com>
2024-05-29 23:43:51 -0500

    loading environment variables

--- apps/worker.cfr-processor/src/main.ts
+++ apps/worker.cfr-processor/src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { WorkerCfrProcessorModule } from './worker.cfr-processor.module';
 import { CFRProcessService } from './service/cfr-processor.service';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const appContext = await NestFactory.createApplicationContext(WorkerCfrProcessorModule);
